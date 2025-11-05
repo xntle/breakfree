1df4389e76a90d918600cd7a182bc0a16382a0bb
An Le <115895584+xntle@users.noreply.github.com>
2025-11-04 17:09:19 -0800

    s

---
+++ frontend/App.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+import './global.css';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-thin">are you ready to</Text>
+      <Text className="text-3xl font-bold ">breakfree</Text>
+      <Text className="mt-10 text-xl font-thin">yes</Text>
+    </View>
+  );
+}
